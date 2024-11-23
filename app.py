4485e49a7a91c2d3cf7a3f4213a5b7990d3816db
Quang Vinh <koncakbo@gmail.com>
2024-11-23 10:37:23 +0700

    plot map

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import requests
+from streamlit_folium import st_folium
 
 
 # page settings
@@ -72,40 +73,153 @@ if selected_page == "Dashboard":
 
     current_time = datetime(2016, 5, 2, 0, 0, 0)
     st.write(f"Current Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    
+    # df_by_city = df_accidents.groupby("City").agg(
+    #         Count=("Severity", "size"),
+    #         Avg_Severity=("Severity", "mean"),
+    #         Lat=("Start_Lat", "mean"),
+    #         Lng=("Start_Lng", "mean"),
+    #     ).reset_index()
+    
+    # df_by_county = df_accidents.groupby("County").agg(
+    #         Count=("Severity", "size"),
+    #         Avg_Severity=("Severity", "mean"),
+    #         Lat=("Start_Lat", "mean"),
+    #         Lng=("Start_Lng", "mean"),
+    #     ).reset_index()
+
+    #     # Aggregating data by State
+    # df_by_state = df_accidents.groupby("State").agg(
+    #     Count=("Severity", "size"),
+    #     Avg_Severity=("Severity", "mean"),
+    #     Lat=("Start_Lat", "mean"),
+    #     Lng=("Start_Lng", "mean"),
+    # ).reset_index()
+
+    # # Simulate zoom level
+    # zoom_level = st.slider("Select Zoom Level", 1, 10, 5)
+
+    # # Choose the dataset to display based on zoom level
+    # if zoom_level >= 7:
+    #     # Show data aggregated by city
+    #     data_to_plot = df_by_city
+    #     lat, lon, color, size, hover_name = "Lat", "Lng", "Avg_Severity", "Count", "City"
+    # elif zoom_level >= 4:
+    #     # Show data aggregated by county
+    #     data_to_plot = df_by_county
+    #     lat, lon, color, size, hover_name = "Lat", "Lng", "Avg_Severity", "Count", "County"
+    # elif zoom_level == 10:
+    #     # Show individual accidents for higher zoom levels
+    #     data_to_plot = df_accidents
+    #     lat, lon, color, size, hover_name = "Start_Lat", "Start_Lng", "Severity", "Severity", "Start_Time"
+    # else:
+    #     # Show data aggregated by state
+    #     data_to_plot = df_by_state
+    #     lat, lon, color, size, hover_name = "Lat", "Lng", "Avg_Severity", "Count", "State"
+    
+    df_loc = df_accidents.loc[(~df_accidents.Start_Lat.isna()) & (~df_accidents.Start_Lng.isna())]
+    
+    def create_map(df_loc, latitude, longitude, zoom, tiles='OpenStreetMap'):
+        """
+        Generate a Folium Map with clustered markers of accident locations.
+        """
+        world_map = folium.Map(location=[latitude, longitude], zoom_start=zoom, tiles=tiles)
+        marker_cluster = MarkerCluster().add_to(world_map)
+
+        # Iterate over the DataFrame rows and add each marker to the cluster
+        for idx, row in df_loc.iterrows():
+            folium.Marker(
+                location=[row['Start_Lat'], row['Start_Lng']],
+                # You can add more attributes to your marker here, such as a popup
+                popup=f"Lat, Lng: {row['Start_Lat']}, {row['Start_Lng']}"
+            ).add_to(marker_cluster)
+
+        return world_map
+    
+    # Coordinates of the US cities
+    us_cities_coords = {
+        "New York": {"lat": 40.7128, "lon": -74.0060},
+        "Los Angeles": {"lat": 34.0522, "lon": -118.2437},
+        "Chicago": {"lat": 41.8781, "lon": -87.6298},
+        "Houston": {"lat": 29.7604, "lon": -95.3698},
+        "Phoenix": {"lat": 33.4484, "lon": -112.0740},
+        "Philadelphia": {"lat": 39.9526, "lon": -75.1652},
+        "San Antonio": {"lat": 29.4241, "lon": -98.4936},
+        "San Diego": {"lat": 32.7157, "lon": -117.1611},
+        "Dallas": {"lat": 32.7767, "lon": -96.7970},
+        "San Jose": {"lat": 37.3382, "lon": -121.8863},
+        "Austin": {"lat": 30.2672, "lon": -97.7431},
+        "Jacksonville": {"lat": 30.3322, "lon": -81.6557},
+        "Fort Worth": {"lat": 32.7555, "lon": -97.3308},
+        "Columbus": {"lat": 39.9612, "lon": -82.9988},
+        "San Francisco": {"lat": 37.7749, "lon": -122.4194},
+        "Charlotte": {"lat": 35.2271, "lon": -80.8431},
+        "Indianapolis": {"lat": 39.7684, "lon": -86.1581},
+        "Seattle": {"lat": 47.6062, "lon": -122.3321},
+        "Denver": {"lat": 39.7392, "lon": -104.9903},
+        "Washington": {"lat": 38.9072, "lon": -77.0369}
+    }
+
+    map_us = create_map(df_loc, 39.50, -98.35, 4)
+    st_folium(map_us, width=1000, height=800)
+    
+    # for seconds in range(10):
+    # #while True:
+    #     # prepare data, dataframe and variables for all visualization
+    #     #Create and display a Folium map with clustered markers for accident locations 
 
 
-    for seconds in range(180):
-    #while True:
-        # prepare data, dataframe and variables for all visualization
+    #     #### Total accident ####
+    #     # Filter data for the selected day
+    #     df_accidents['Start_Time'] = pd.to_datetime(df_accidents['Start_Time'], format='mixed')
+    #     current_day = current_time.date()
+    #     previous_day = (current_time - pd.Timedelta(days=2)).date()
+
+    #     # Get accidents for the current day
+    #     current_day_accidents = df_accidents[df_accidents['Start_Time'].dt.date == current_day]
+    #     total_current_day = len(current_day_accidents)
+
+    #     # Get accidents for the previous day
+    #     previous_day_accidents = df_accidents[df_accidents['Start_Time'].dt.date == previous_day]
+    #     total_previous_day = len(previous_day_accidents)
         
-        # Filter data for the selected day
-        df_accidents['Start_Time'] = pd.to_datetime(df_accidents['Start_Time'])
-        current_day = current_time.date()
-        previous_day = (current_time - pd.Timedelta(days=1)).date()
-
-        # Get accidents for the current day
-        current_day_accidents = df_accidents[df_accidents['Start_Time'].dt.date == current_day]
-        total_current_day = len(current_day_accidents)
-
-        # Get accidents for the previous day
-        previous_day_accidents = df_accidents[df_accidents['Start_Time'].dt.date == previous_day]
-        total_previous_day = len(previous_day_accidents)
-
-        # Calculate percentage increase
-        if total_previous_day == 0:
-            percent_increase = "N/A (No accidents on the previous day)"
-        else:
-            percent_increase = ((total_current_day - total_previous_day) / total_previous_day) * 100
+    #     # Calculate percentage increase
+    #     if total_previous_day == 0:
+    #         percent_increase = "N/A (No accidents on the previous day)"
+    #     else:
+    #         percent_increase = f"{((total_current_day - total_previous_day) / total_previous_day) * 100}%"
         
-        with placeholder.container():
+    #     #### Most accident city ####
+
+
+    #     with placeholder.container():
             #visualize
             # Display metrics
-            st.metric(label="Total Accidents Today", value=total_current_day)
-            st.metric(label="Total Accidents Yesterday", value=total_previous_day)
-            if isinstance(percent_increase, str):
-                st.write(f"Percentage Increase: {percent_increase}")
-            else:
-                st.metric(label="Percentage Increase from Yesterday", value=f"{percent_increase:.2f}%")
+            # kp1, kp2, kp3 = st.columns(3)
+            
+            # kp1.metric(label="Total Accidents Today", value=total_current_day, delta=percent_increase)
+            # kp2.metric()
+            # kp3.metric()
+
+
+            # Create scatter mapbox chart
+            # fig = px.scatter_mapbox(
+            #     data_to_plot,
+            #     lat=lat,
+            #     lon=lon,
+            #     color=color,
+            #     size=size,
+            #     zoom=zoom_level,
+            #     hover_name=hover_name,
+            #     height=900,
+            #     color_continuous_scale=px.colors.sequential.Viridis,
+            # )
+            
+            # fig.update_layout(mapbox_style="open-street-map")
+            # fig.update_layout(title="All Accidents Overview")
+            # st.plotly_chart(fig, use_container_width=True, key=f"scatter_mapbox_chart_{seconds}")
+                
+                
 
 
 
